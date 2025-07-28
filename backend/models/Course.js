f8fab34b04368210fabe49ca845036f4eeb40263
baqar-abbas <baqar.abbas@hotmail.com>
2025-07-28 06:43:39 +0500

    Create sequelize models and schemas for User, course and lesson

---
+++ backend/models/Course.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Course = sequelize.define(
+  "Course",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    status: {
+      type: DataTypes.ENUM("published", "draft"),
+      defaultValue: "published",
+    },
+  },
+  {
+    tableName: "courses",
+    timestamps: true,
+  }
+);
+
+module.exports = Course;
---
+++ backend/models/Lesson.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Lesson = sequelize.define(
+  "Lesson",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    order: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "lessons",
+    timestamps: true,
+  }
+);
+
+module.exports = Lesson;
