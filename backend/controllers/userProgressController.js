fac5e1adc590099f9430118d59c08c6dde465793
baqar-abbas <baqar.abbas@hotmail.com>
2025-10-30 13:52:50 +0500

    Create controller method for get progress for a logged in user

--- backend/controllers/userProgressController.js
+++ backend/controllers/userProgressController.js
@@ -2,13 +2,27 @@ const { UserProgress, Lesson, User } = require("../models");
 
 // Controller to handle user progress
 
-exports.getAllProgress = async (req, resizeBy, next) => {
+// Find all user progress records
+exports.getAllProgress = async (req, res, next) => {
   try {
     const progress = await UserProgress.findAll({
       include: [
         { model: User, attributes: ["id", "name", "email"] },
         { model: Lesson, attributes: ["id", "title"] },
       ],
+    });
+    res.status(200).json(progress);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get progress for the logged-in user
+exports.getMyProgress = async (req, res, next) => {
+  try {
+    const progress = await UserProgress.findAll({
+      where: { userId: req.user.id },
+      include: [{ model: Lesson, attributes: ["id", "title"] }],
     });
     res.status(200).json(progress);
   } catch (error) {
