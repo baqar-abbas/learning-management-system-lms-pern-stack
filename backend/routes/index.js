7c9b7140c22e9bdc286714ba04f62ccd689d09fa
baqar-abbas <baqar.abbas@hotmail.com>
2025-10-30 15:44:54 +0500

    Mount progress routes at the top level on index.js file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,13 +4,11 @@ const router = express.Router();
 // Import route modules
 const authRoutes = require("./authRoutes");
 const courseRoutes = require("./courseRoutes");
-// const lessonRoutes = require('./lessonRoutes'); // Add later
-// const quizRoutes = require('./quizRoutes');     // Add later
+const userProgressRoutes = require("./userProgressRoutes");
 
 // Mount routes
 router.use("/auth", authRoutes);
 router.use("/courses", courseRoutes);
-// router.use('/lessons', lessonRoutes);
-// router.use('/quizzes', quizRoutes);
+router.use("/progress", userProgressRoutes);
 
 module.exports = router;
