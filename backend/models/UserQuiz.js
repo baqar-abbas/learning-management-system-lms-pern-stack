870e6faa3237bb1a488978ec2778b64a09a7eab4
baqar-abbas <baqar.abbas@hotmail.com>
2025-12-12 16:31:48 +0500

    Create new model User Quiz for quiz scores

---
+++ backend/models/UserQuiz.js
@@ -0,0 +1,46 @@
+const { sequelize } = require("../config/database");
+
+// models/userQuiz.js
+module.exports = (sequelize, DataTypes) => {
+  const UserQuiz = sequelize.define(
+    "UserQuiz",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      quizId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      score: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      total: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      passed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      // store answers as JSON: [{ optionId, isCorrect }, ...]
+      answers: {
+        type: DataTypes.JSONB || DataTypes.JSON,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "user_quizzes",
+      timestamps: true,
+    }
+  );
+};
